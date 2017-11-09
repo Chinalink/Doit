2d1ba1e228a9dd99e39106ea71f2baaca2937914
HuXiaoDai <416681736@qq.com>
2017-11-09 18:42:42 +0800

    '动画修改'

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <transition enter-active-class="animated fadeIn" leave-active-class="animated fadeOut">
-      <router-view></router-view>
+      <router-view class="page-warp"></router-view>
     </transition>
   </div>
 </template>
@@ -14,3 +14,13 @@
     }
   }
 </script>
+
+<style>
+  .page-warp {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+  }
+</style>
